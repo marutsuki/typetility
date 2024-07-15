291ef647132c4c602c2c74f9ef3123c812da6699
lucien7789 <68990764+lucien7789@users.noreply.github.com>
2024-07-16 01:08:50 +1000

    prettier + eslint

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import dts from 'rollup-plugin-dts';
 export default [
     {
         input: 'src/index.d.ts',
-        output: [{ file: 'dist/index.d.ts', format: "esm" }],
+        output: [{ file: 'dist/index.d.ts', format: 'esm' }],
         plugins: [dts()],
     },
 ];
