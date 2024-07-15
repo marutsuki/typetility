21a1177bbd0963de0f6f06f85adbef6d6bbf69c8
lucien7789 <68990764+lucien7789@users.noreply.github.com>
2024-07-16 01:36:56 +1000

    Incomplete type

---
+++ src/examples/Incomplete.ts
@@ -0,0 +1,30 @@
+import { Incomplete } from '..';
+
+type Obj = {
+    a: string;
+    b?: string;
+    c: {
+        d: number[];
+        e?: boolean;
+        f: {
+            g?: string;
+        };
+    };
+};
+
+/**
+ * All nested properties can be set to ```undefined```.
+ */
+const test: Incomplete<Obj> = {
+    a: undefined,
+    b: undefined,
+    c: {
+        d: undefined,
+        e: undefined,
+        f: {
+            g: undefined,
+        },
+    },
+};
+
+console.info(test);
